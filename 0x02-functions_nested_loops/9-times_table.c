e58a0ae48e3dc30668fb6abf8eb2c08db17e1b59
Joyce <jmalisha22@gmail.com>
2022-03-15 14:51:35 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -34,8 +34,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(tens + '0');
-				_putchar(ones + '0');
+				_putchar((ones / 10) + '0');
+				_putchar((ones  % 10) + '0');
 			}
 		}
 		_putchar('\n');
