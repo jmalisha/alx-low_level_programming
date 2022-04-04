75f84630c9a566c2532c036f3314a5209e8c4d24
Joyce <jmalisha22@gmail.com>
2022-04-04 05:10:57 -0700

    libmy.a

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    	return (0);
 }
